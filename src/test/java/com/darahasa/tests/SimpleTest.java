b7f86d21c5be9fe3524fafe20c5c1cfb358c931a
Darahasa 1512 <minnekantidarahasa@gmail.com>
2026-01-19 21:35:16 -0600

    Initial commit

---
+++ src/test/java/com/darahasa/tests/SimpleTest.java
@@ -0,0 +1,19 @@
+package com.darahasa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void thisTestShouldPass() {
+        System.out.println("TEST EXECUTED - PASS");
+        Assert.assertEquals(2 + 2, 4);
+    }
+
+    @Test
+    public void thisTestShouldFail() {
+        System.out.println("TEST EXECUTED - FAIL");
+        Assert.assertEquals(2 + 2, 5);
+    }
+}
